8ffca782209449f1ea10fcc378a34c9ae25397e1
Natasa <naleeekg@gmail.com>
2017-09-09 16:46:30 -0700

    Final touchups

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+// Ajax call for getting hotels list
+
 function loadDoc() {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200){
             var obj = JSON.parse(this.responseText);
             document.getElementById("hotels").innerHTML = displayHotels(obj);
-        }else if(this.status == 500){
+        }else if(this.status == 500){ //in case of a server error, display error message
             var obj = JSON.parse(this.responseText);
             document.getElementById("hotels").innerHTML = "<div class=\"alert alert-danger\"><i class=\"fa fa-frown-o fa-2x\" aria-hidden=\"true\"></i>&nbsp;&nbsp;" + obj.error + "</div>";
         }
@@ -12,12 +14,15 @@ function loadDoc() {
     xhttp.open("GET","http://fake-hotel-api.herokuapp.com/api/hotels?count=5", true);
     xhttp.send();
 };
+
+// Ajax call for getting user reviews
+
 function showReviews(id) {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200){
             var obj = JSON.parse(this.responseText);
-            if(obj.length > 0){
+            if(obj.length > 0){ //if object is empty, display an error message
                 document.getElementById(id).innerHTML = singleReview(obj);
             }else{
                 document.getElementById(id).innerHTML = "<div class=\"alert alert-warning\"><i class=\"fa fa-smile-o fa-2x\" aria-hidden=\"true\"></i> This hotel has no reviews.</div>";
@@ -31,38 +36,40 @@ function showReviews(id) {
     xhttp.open("GET","http://fake-hotel-api.herokuapp.com/api/reviews?hotel_id="+ id, true);
     xhttp.send();
 };
+
+// Toggle the user reviews container
+
 function toogleView(reviewBox, buttonElement) {
 
     var x = document.getElementById(reviewBox);
-    //var buttonNewText = document.getElementById(buttonOriginalText);
     if (x.style.display === 'block') {
         x.style.display = 'none';
-        //buttonNewText.value = "Show Reviews";
         buttonElement.value = "Show Reviews";
     } else {
         x.style.display = 'block';
         buttonElement.value = "Hide Reviews";
-        //buttonNewText.replace('Hide Reviews','Show Reviews');
     }
 };
 
-function displayHotels(pera){
+// Display hotels function
+
+function displayHotels(hoteList){
     table="";
     var i;
-    for (i=0 ; i< pera.length; i++){
-        var hotel = pera[i];
+    for (i=0 ; i< hoteList.length; i++){
+        var hotel = hoteList[i];
         var stars="";
         var j;
-        for(j=0;j<hotel.stars;j++){
+        for(j=0;j<hotel.stars;j++){ //Showing rating starts for each hotel
             stars+= "<i class=\"fa fa-star\"></i>";
         }
         for(j=0;j<(5- hotel.stars);j++){
             stars+= "<i class=\"fa fa-star-o\"></i>";
         }
+        // Loading each hotel
         table +=
             " <div class=\"row panel\" id=\"hotel\">\n" +
-            "                <div class=\"rating\">\n" +
-            stars+
+            "                <div class=\"rating\">\n" + stars+
             "                </div>\n" +
             "                <div class=\"imageHolder col-md-4 \">\n" +
             "                    <img class=\"hotelImg\" src=" + hotel.images +">\n" +
@@ -80,8 +87,8 @@ function displayHotels(pera){
             "            </div>\n" +
             "            <div class=\"jumbotron\" id="+hotel.id+"\>\n" +
             "                        \n" +
-            "            </div>\n" +
-            "\n" +
+            "            </div>\n"+
+            //hardcoded part for preview/design purposes
             "            <div class=\"row nav\">\n" +
             "                <div class=\"col-md-4\"></div>\n" +
             "                <div class=\"col-md-8 col-xs-12\" style=\"margin: 0px;padding: 0px;\">\n" +
@@ -94,13 +101,15 @@ function displayHotels(pera){
     return table;
 };
 
+// Loading hotel reviews
+
 function singleReview(obj){
     review= "";
     var j;
     for(j=0; j<obj.length; j++){
         var reviewer = obj[j];
         feedback = "";
-        if(reviewer.positive){
+        if(reviewer.positive){ // showing users feedback
             feedback = "<i class=\"fa fa-smile-o\"></i>"
         }
         else{
@@ -118,6 +127,8 @@ function singleReview(obj){
     }
     return review;
 };
+
+// Parsing date string into german date format
 
 function parseDate(dateString) {
     var date = new Date(dateString);// This creates a Date object with the date value represented by a date string .
